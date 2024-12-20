33135379695693c4413d18c7ab65f4e0ac1520c4
Kappasalah <kappasalah03@gmail.com>
2024-12-20 16:21:27 +0000

    hello.c agg

--- hello/hello.c
+++ hello/hello.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
 int main()
-{ 
-    printf("hello world!!!\n\n\n");
+{
+    // Declare a variable to store the name (32 characters max)
+    char name[32];
 
+    // Print the "Hello World" message
+    printf("Hello World!!!\n\n\n");
+
+    // Ask for the name
+    printf("Enter a name: ");
+    
+    // Read the user's input
+    scanf("%s", name);
+
+    // Print a message using the name entered
+    printf("\nHello\t%s\n\n\n", name);
+
+    // Return 0 to indicate successful program termination
     return 0;
 }
